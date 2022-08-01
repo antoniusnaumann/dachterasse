a4290a6a59a181af003813954479d31fe49803af
Antonius Naumann <antonius.naumann@student.hpi.de>
2022-08-01 12:39:12 +0200

    Export all source structs under sources namespace

--- src/lib.rs
+++ src/lib.rs
@@ -19,21 +19,25 @@ mod lectures {
 
     pub mod sources {
         pub mod filesystem_source;
-        pub mod scraper_source;
         pub mod memory_source;
+        pub mod scraper_source;
+
+        pub use filesystem_source::FSDataSource;
+        pub use memory_source::InMemoryDataSource;
+        pub use scraper_source::ScraperSource;
     }
 
-    pub mod config;
     pub mod client;
+    pub mod config;
 }
 
 pub use crate::lectures::client::LectureClient;
 pub use crate::lectures::config::Config;
-pub use crate::lectures::entities::Lecture;
-pub use crate::lectures::entities::Degrees;
 pub use crate::lectures::entities::Degree;
+pub use crate::lectures::entities::Degrees;
+pub use crate::lectures::entities::Lecture;
 
-pub use crate::lectures::datasource as datasource;
-pub use crate::lectures::repository as repository;
-pub use crate::lectures::scrape as scrape;
-pub use crate::lectures::sources as sources;
+pub use crate::lectures::datasource;
+pub use crate::lectures::repository;
+pub use crate::lectures::scrape;
+pub use crate::lectures::sources;
